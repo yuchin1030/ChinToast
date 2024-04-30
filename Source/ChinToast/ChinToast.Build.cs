28f0e9f6ddda20006cc0bdd7f857bc8a59c0e094
KSW1999 <kso8242@naver.com>
2024-04-30 16:48:04 +0900

    [Add] 재경이 파일 추가

--- Source/ChinToast/ChinToast.Build.cs
+++ Source/ChinToast/ChinToast.Build.cs
@@ -8,7 +8,7 @@ public class ChinToast : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
