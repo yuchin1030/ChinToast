5a7099e6b8ae736782a27740de914653e8bba43e
KSW1999 <kso8242@naver.com>
2024-04-30 19:25:10 +0900

    [Update] 손님 랜덤화
    손님이 소환될 때 지정된 매쉬중 랜덤으로 1개의 매쉬를 선택하여 나타나도록 수정

--- Source/ChinToast/Private/Customer.cpp
+++ Source/ChinToast/Private/Customer.cpp
@@ -6,23 +6,89 @@ ACustomer::ACustomer()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+
+
 }
 
 void ACustomer::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	int32 ranNum = FMath::RandRange(0,2);
+
+	GetMesh()->SetSkeletalMesh(bodys[ranNum]);
 }
 
 void ACustomer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	switch (state)
+	{
+	case ECustomerState::IDLE:
+		Idle();
+		break;
+	case ECustomerState::MOVEIN:
+		MoveIn();
+		break;
+	case ECustomerState::ORDER:
+		Order();
+		break;
+	case ECustomerState::ORDERDELAY:
+		Wait();
+		break;
+	case ECustomerState::CHECK:
+		Check();
+		break;
+	case ECustomerState::PAYMENT:
+		Payment();
+		break;
+	case ECustomerState::MOVEOUT:
+		MoveOut();
+		break;
+	default:
+		break;
+	}
 }
 
 void ACustomer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ACustomer::Idle()
+{
+
+}
+
+void ACustomer::MoveIn()
+{
+
+}
+
+void ACustomer::Order()
+{
+
+}
+
+void ACustomer::Wait()
+{
+
+}
+
+void ACustomer::Check()
+{
+
+}
+
+void ACustomer::Payment()
+{
+
+}
+
+void ACustomer::MoveOut()
+{
 
 }
 
--- Source/ChinToast/Public/Customer.h
+++ Source/ChinToast/Public/Customer.h
@@ -5,6 +5,17 @@
 #include "GameFramework/Character.h"
 #include "Customer.generated.h"
 
+UENUM(BlueprintType)
+enum class ECustomerState : uint8
+{
+	IDLE		UMETA(DisplayName = "Idle State"),
+	MOVEIN		UMETA(DisplayName = "Move In State"),
+	ORDER		UMETA(DisplayName = "Order State"),
+	ORDERDELAY	UMETA(DisplayName = "Order Delay State"),
+	CHECK		UMETA(DisplayName = "Check State"),
+	PAYMENT		UMETA(DisplayName = "Pay State"),
+	MOVEOUT		UMETA(DisplayName = "Move Out State"),
+};
 UCLASS()
 class CHINTOAST_API ACustomer : public ACharacter
 {
@@ -21,4 +32,39 @@ public:
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// 고객 종류에 따라 바뀔 메쉬 저장용
+	UPROPERTY(EditDefaultsOnly)
+	TArray <USkeletalMesh*> bodys;
+
+	// 손에 들고갈 샌드위치 매쉬
+	UPROPERTY(EditDefaultsOnly)
+	class UStaticMeshComponent* sandwich;
+
+	// 수령확인 후 인사 애니몽타주
+	UPROPERTY(EditDefaultsOnly)
+	TArray <UAnimMontage*> thanks;
+
+	// FSM 상태변수
+	UPROPERTY(EditDefaultsOnly)
+	ECustomerState state = ECustomerState::IDLE;
+
+	// 주문할 샌드위치 레시피 재료 순서
+	UPROPERTY(EditAnywhere)
+	TArray<UStaticMesh*> recipe;
+
+	// 고객 종류(매쉬 타입)
+	UPROPERTY(EditAnywhere)
+	int32 customerType = 0;
+
+private:
+
+	float ticktime = 0.0f;
+	
+	void Idle();
+	void MoveIn();
+	void Order();
+	void Wait();
+	void Check();
+	void Payment();
+	void MoveOut();
 };
